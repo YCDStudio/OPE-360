51ae6772d960fcb5a4f2d053507ef5db5d58f5d9
YCDStudio <ycd.studio.ops@gmail.com>
2025-11-07 10:01:36 +0300

    initial upload

--- data.js
+++ data.js
@@ -30,8 +30,8 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.4089217169644996,
-          "pitch": 0.014958308094703554,
+          "yaw": 1.4273088238463938,
+          "pitch": 0.0493275660515593,
           "rotation": 0,
           "target": "2-ope_360_03"
         }
@@ -68,10 +68,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.83289293530021,
-          "pitch": 0.021552717235108432,
+          "yaw": -1.5112729541831733,
+          "pitch": 0.008120178550672819,
           "rotation": 0,
           "target": "2-ope_360_03"
+        },
+        {
+          "yaw": 1.9787127497777144,
+          "pitch": -0.025646427860202436,
+          "rotation": 0,
+          "target": "3-ope_360_04"
         }
       ],
       "infoHotspots": []
@@ -106,14 +112,52 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.4982230240995449,
-          "pitch": 0.19331678904616467,
+          "yaw": -1.4860908526751544,
+          "pitch": 0.08809779729001832,
           "rotation": 0,
           "target": "0-ope_360_01"
         },
         {
-          "yaw": 1.0635929598815643,
-          "pitch": 0.05042238568158908,
+          "yaw": 1.8108615548192635,
+          "pitch": 0.06931102157941993,
+          "rotation": 0,
+          "target": "1-ope_360_02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ope_360_04",
+      "name": "OPE_360_04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5706137254369956,
+          "pitch": 0.12062726290305292,
           "rotation": 0,
           "target": "1-ope_360_02"
         }
@@ -121,7 +165,7 @@ var APP_DATA = {
       "infoHotspots": []
     }
   ],
-  "name": "OPE",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
